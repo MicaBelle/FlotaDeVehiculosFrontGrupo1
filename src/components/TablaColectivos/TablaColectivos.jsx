1e07b864ea6349ca1109fd31b0052702e7f3f5e1
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-10-04 16:42:13 -0300

    el requeriemitnos 1.2 y 1.3 esta hecho, falta hacer el llamdo a la api para cambiar el estado

--- src/components/TablaColectivos/TablaColectivos.jsx
+++ src/components/TablaColectivos/TablaColectivos.jsx
@@ -10,8 +10,7 @@ import {
   Button,
   Chip,
 } from "@nextui-org/react";
-import { VerDetalleColectivo } from '../VerDetalleColectivo/VerDetalleColectivo';
-import { AsignarOperador } from '../AsignarOperador/AsignarOperador';
+import { VerDetalleColectivo } from "../VerDetalleColectivo/VerDetalleColectivo";
 
 const columns = [
   { uid: "patente", name: "PATENTE" },
@@ -20,7 +19,7 @@ const columns = [
   { uid: "kilometraje", name: "KILOMETRAJE" },
   { uid: "litrosTanque", name: "LITROS DE TANQUE" },
   { uid: "estado", name: "ESTADO" },
-  { uid: "chofer", name: "CHOFER" },
+  { uid: "fechaDeRevison", name: "FECHA DE REVISION" },
   { uid: "actions", name: "ACCIONES" },
 ];
 
@@ -29,22 +28,14 @@ export function TablaDeColectivos({ userRole }) {
   const [selectedColectivo, setSelectedColectivo] = useState(null);
   const [filterValue, setFilterValue] = useState("");
   const [filterStatus, setFilterStatus] = useState("all");
-  const [asignarOperador, setAsignarOperador] = useState(false);
-  const [colectivoParaAsignar, setColectivoParaAsignar] = useState(null); 
 
   useEffect(() => {
+  
     const mockData = [
-      { patente: "ABC123", chasis: "XYZ456", antiguedad: 5, kilometraje: 150000, litrosTanque: 800, estado: "No disponible", chofer: "Juan Pérez" },
-      { patente: "DEF456", chasis: "UVW789", antiguedad: 3, kilometraje: 80000, litrosTanque: 800, estado: "Disponible", chofer: "Ana Gómez" },
-      { patente: "GHI789", chasis: "RST012", antiguedad: 8, kilometraje: 200000, litrosTanque: 800, estado: "No disponible", chofer: "Carlos Martínez" },
-      { patente: "JKL012", chasis: "OPQ345", antiguedad: 2, kilometraje: 50000, litrosTanque: 800, estado: "Disponible", chofer: "Lucía Fernández" },
-      { patente: "ABC123", chasis: "XYZ456", antiguedad: 5, kilometraje: 150000, litrosTanque: 800, estado: "No disponible", chofer: "Juan Pérez" },
-      { patente: "DEF456", chasis: "UVW789", antiguedad: 3, kilometraje: 80000, litrosTanque: 800, estado: "Disponible", chofer: "Ana Gómez" },
-      { patente: "GHI789", chasis: "RST012", antiguedad: 8, kilometraje: 200000, litrosTanque: 800, estado: "No disponible", chofer: "Carlos Martínez" },
-      { patente: "JKL012", chasis: "OPQ345", antiguedad: 2, kilometraje: 50000, litrosTanque: 800, estado: "Disponible", chofer: "Lucía Fernández" },
-      { patente: "ABC123", chasis: "XYZ456", antiguedad: 5, kilometraje: 150000, litrosTanque: 800, estado: "No disponible", chofer: "Juan Pérez" },
-      { patente: "ABC123", chasis: "XYZ456", antiguedad: 5, kilometraje: 150000, litrosTanque: 800, estado: "No disponible", chofer: "Juan Pérez" },
-      { patente: "DEF456", chasis: "UVW789", antiguedad: 3, kilometraje: 80000, litrosTanque: 800, estado: "Disponible", chofer: "Ana Gómez" },  
+      { patente: "ABC123", chasis: "XYZ456", antiguedad: 5, kilometraje: 150000, litrosTanque: 800, estado: "Inhabilitado", fechaDeRevison: "10/2/24" },
+      { patente: "DEF456", chasis: "UVW789", antiguedad: 3, kilometraje: 80000, litrosTanque: 800, estado: "Habilitado", fechaDeRevison: "15/09/24" },
+      { patente: "GHI789", chasis: "RST012", antiguedad: 8, kilometraje: 200000, litrosTanque: 800, estado: "Habilitado", fechaDeRevison: "13/10/24" },
+      { patente: "JKL012", chasis: "OPQ345", antiguedad: 2, kilometraje: 50000, litrosTanque: 800, estado: "Inhabilitado", fechaDeRevison: "18/01/24" },
     ];
 
     const mappedRows = mockData.map((item, index) => ({
@@ -63,16 +54,23 @@ export function TablaDeColectivos({ userRole }) {
     setSelectedColectivo(null);
   };
 
-  const handleEditar = () => {
-    alert('Función para editar colectivo');
-  };
-
   const handleFilterByStatus = (status) => {
     setFilterStatus(status);
   };
 
+  const handleToggleEstado = (item) => {
+    const newState = item.estado === "Habilitado" ? "Inhabilitado" : "Habilitado";
+    //llamada a la api con elestado
+    
+    setFilas((prevRows) =>
+      prevRows.map((row) =>
+        row.patente === item.patente ? { ...row, estado: newState } : row
+      )
+    );
+  };
+
   const filteredRows = useMemo(() => {
-    return filas.filter(row => 
+    return filas.filter((row) =>
       (filterStatus === "all" || row.estado === filterStatus) &&
       row.patente.toLowerCase().includes(filterValue.toLowerCase())
     );
@@ -90,8 +88,8 @@ export function TablaDeColectivos({ userRole }) {
       />
       <div className="flex gap-2">
         <Button onClick={() => handleFilterByStatus("all")}>Todos</Button>
-        <Button onClick={() => handleFilterByStatus("Disponible")}>Disponibles</Button>
-        <Button onClick={() => handleFilterByStatus("No disponible")}>No Disponibles</Button>
+        <Button onClick={() => handleFilterByStatus("Habilitado")}>Habilitados</Button>
+        <Button onClick={() => handleFilterByStatus("Inhabilitado")}>Inhabilitados</Button>
       </div>
     </div>
   );
@@ -102,7 +100,12 @@ export function TablaDeColectivos({ userRole }) {
     switch (columnKey) {
       case "estado":
         return (
-          <Chip className="capitalize" color={cellValue === "Disponible" ? "success" : "danger"} size="sm" variant="flat">
+          <Chip
+            className="capitalize"
+            color={cellValue === "Habilitado" ? "success" : "danger"}
+            size="sm"
+            variant="flat"
+          >
             {cellValue}
           </Chip>
         );
@@ -111,16 +114,11 @@ export function TablaDeColectivos({ userRole }) {
           <div className="flex justify-end items-center gap-2">
             <Button onClick={() => handleVerDetalle(item)}>Ver detalle</Button>
             {userRole === "admin" && (
-              <Button color="danger" onClick={() => console.log(`Eliminar: ${item.patente}`)}>Eliminar</Button>
-            )}
-            {item.estado === "No disponible" && (
-            <Button 
-              color="warning" 
-              onClick={() => { 
-                setColectivoParaAsignar(item); 
-                setAsignarOperador(true); 
-              }}>
-                Asignar Operador
+              <Button
+                color={item.estado === "Habilitado" ? "danger" : "success"}
+                onClick={() => handleToggleEstado(item)}
+              >
+                {item.estado === "Habilitado" ? "Inhabilitar" : "Habilitar"}
               </Button>
             )}
           </div>
@@ -132,10 +130,11 @@ export function TablaDeColectivos({ userRole }) {
 
   return (
     <div>
-      {asignarOperador ? ( 
-        <AsignarOperador colectivo={colectivoParaAsignar} onClose={() => setAsignarOperador(false)} />
-      ) : selectedColectivo ? ( 
-        <VerDetalleColectivo colectivo={selectedColectivo} irAtras={handleIrAtras} editar={handleEditar} />
+      {selectedColectivo ? (
+        <VerDetalleColectivo
+          colectivo={selectedColectivo}
+          irAtras={handleIrAtras}
+        />
       ) : (
         <Table
           aria-label="Tabla de Colectivos"
@@ -144,7 +143,10 @@ export function TablaDeColectivos({ userRole }) {
         >
           <TableHeader columns={columns}>
             {(column) => (
-              <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+              <TableColumn
+                key={column.uid}
+                align={column.uid === "actions" ? "center" : "start"}
+              >
                 {column.name}
               </TableColumn>
             )}
@@ -164,34 +166,29 @@ export function TablaDeColectivos({ userRole }) {
 
 export default TablaDeColectivos;
 
+/* useEffect(() => {
+   const fetchData = async () => {
+     try {
+       const response = await axios.get("/api/colectivos");
+       const data = response.data;
 
+       const mappedRows = data.map((item, index) => ({
+         key: index.toString(),
+         patente: item.patente,
+         chasis: item.chasis,
+         antiguedad: item.antiguedad,
+         kilometraje: item.kilometraje,
+         litrosTanque: item.litrosTanque || 800,
+         estado: item.estado || "Desconocido",
+         chofer: item.chofer || "Sin asignar",
+         //iria la fecha de revision
+       }));
 
+       setFilas(mappedRows);
+     } catch (error) {
+       console.error("Error fetching data: ", error);
+     }
+   };
 
-/*export function TablaDeColectivos() {
-const [rows, setRows] = useState([]);
-
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('/api/colectivos'); 
-      const data = response.data;
-
-      
-      const mappedRows = data.map((item, index) => ({
-        key: index.toString(),
-        patente: item.patente,
-        chasis: item.chasis,
-        antiguedad: item.antiguedad,
-        kilometraje: item.kilometraje,
-        litrosTanque: item.litrosTanque || 800,
-        chofer: item.chofer,
-      }));
-
-      setRows(mappedRows);
-    } catch (error) {
-      console.error("Error fetching data: ", error);
-    }
-  };
-
-  fetchData();
-}, []);*/
+   fetchData();
+ }, []);*/
