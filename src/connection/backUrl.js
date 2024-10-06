034fbe49eeadfd0358636494a9e34fd7af9fd0b3
Tomas Ledesma <tomasld13@gmail.com>
2024-10-06 18:31:41 -0300

    fix: refactor ip back

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,3 +1,3 @@
-export const backendUrl = "http://3.80.97.251:8080"; //prod
+export const backendUrl = "http://18.209.5.94:8080"; //prod
 
 //export const backendUrl = "http://localhost:8080"; //local
