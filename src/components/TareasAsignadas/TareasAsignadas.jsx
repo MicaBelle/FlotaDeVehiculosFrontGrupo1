8055c388efbe30c938c4eba6573a6f9e25447525
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-10-09 11:02:37 -0300

    add: agregue otro estilo al ver las tareas del operador

--- src/components/TareasAsignadas/TareasAsignadas.jsx
+++ src/components/TareasAsignadas/TareasAsignadas.jsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardHeader, CardBody, CardFooter, Divider, Image, Button } from "@nextui-org/react";
 import { useSelector } from 'react-redux';
-import mantenimientoImagen from '../../assets/Images/LogoNavBar.jpeg';
-import { verMisMantenimientos, finalizarMantenimiento } from '../../services/mantenimientoService'; 
-import TablaDeInventario from '../RegistroItemInventario/TablaInventario';
+import { verMisMantenimientos } from '../../services/mantenimientoService';
 
+import '../TareasAsignadas/styles/TareasAsignadas.css'
+import TarjetaMantenimiento from './TajetaMantenimiento';
 
 export const TareasAsignadas = () => {
   const [tareas, setTareas] = useState([]);
-  const [showInventory, setShowInventory] = useState(false);
-  const [currentTaskId, setCurrentTaskId] = useState(null);
-  const [selectedItems, setSelectedItems] = useState({});
   const token = useSelector((state) => state.user.token); 
 
   useEffect(() => {
@@ -33,69 +29,24 @@ export const TareasAsignadas = () => {
     }
   }, [token]);
 
-  const handleFinalizarTarea = (tareaId) => {
-    setCurrentTaskId(tareaId); 
-    setShowInventory(true); 
-  };
-
-  const handleConfirmarSeleccion = async (itemsSeleccionados) => {
-    const fechaFinalizacion = new Date().toISOString(); 
-    const data = {
-      fechaFinalizacion,
-      itemsSeleccionados, 
-    };
-
-    try {
-      await finalizarMantenimiento(currentTaskId, data, token);
-      alert("Tarea finalizada y los ítems han sido descontados del stock.");
-      setTareas((prevTareas) => prevTareas.filter(tarea => tarea.id !== currentTaskId));
-      setShowInventory(false); 
-    } catch (error) {
-      alert("Error al finalizar la tarea.");
-    }
+  const handleTareaFinalizada = (tareaId) => {
+    setTareas((prevTareas) => prevTareas.filter(tarea => tarea.id !== tareaId));
   };
 
   return (
-    <div>
-      {showInventory && (
-        <TablaDeInventario 
-          onConfirm={handleConfirmarSeleccion} 
-          onCancel={() => setShowInventory(false)} 
-        />
-      )}
+    <div className="tarjeta-container">
       {tareas.length === 0 ? (
         <p>No hay tareas asignadas</p>
       ) : (
         tareas.map((tarea) => (
-          <Card className="max-w-[400px]" key={tarea.id}>
-            <CardHeader className="flex gap-3">
-              <Image
-                alt="Imagen de la tarea"
-                height={40}
-                radius="sm"
-                src={mantenimientoImagen}
-                width={40}
-              />
-              <div className="flex flex-col">
-                <p className="text-md font-bold">{tarea.asunto}</p>
-              </div>
-            </CardHeader>
-            <Divider />
-            <CardBody>
-              <p>Estado: {tarea.estadoMantenimiento}</p>
-              <p>Operador: {tarea.operador.usuario}</p> 
-              <p>Vehículo: {tarea.vehiculo.patente}</p>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <Button
-                color="secondary"
-                onClick={() => handleFinalizarTarea(tarea.id)}
-              >
-                Finalizar Tarea
-              </Button>
-            </CardFooter>
-          </Card>
+          <div className="tarjeta" key={tarea.id}>
+          <TarjetaMantenimiento 
+            key={tarea.id} 
+            tarea={tarea} 
+            token={token} 
+            onTareaFinalizada={handleTareaFinalizada}
+          />
+        </div>
         ))
       )}
     </div>
