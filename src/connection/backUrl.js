4f83bfb236bb1cc53de13b5a78062f2ccdef7b51
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-10-13 15:37:10 -0300

    Produccion

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,3 +1,3 @@
-//export const backendUrl = "http://18.209.5.94:8080"; //prod
+export const backendUrl = "http://18.209.5.94:8080"; //prod
 
-export const backendUrl = "http://localhost:8080"; //local
+//export const backendUrl = "http://localhost:8080"; //local
