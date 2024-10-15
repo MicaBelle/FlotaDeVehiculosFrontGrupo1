33e18bfc0e2a99b3bfd5d934542fdb8ee0cc9e1e
Tomas Ledesma <tomasld13@gmail.com>
2024-10-15 20:15:03 -0300

    add: rutas chofer

--- src/services/choferesService.js
+++ src/services/choferesService.js
@@ -3,25 +3,49 @@ import {executeFetch} from '../connection/fetch'
 import {HttpMethods} from '../connection/HttpMethods'
 
 
-
+/*
+public class ChoferRegistroDTO {
+    String username;
+    String password;
+    String nombre;
+}
+*/
 export const registrarChofer = async (data, token) => {
     const endpoint = backendUrl + '/chofer/registrar';
-    return await executeFetch(endpoint, data, HttpMethods.GET, token, 200);
+    return await executeFetch(endpoint, data, HttpMethods.POST, token, 201);
 };
 
+/*
+public class AsignarChoferDTO {
+    Integer idVehiculo;
+    Integer idChofer;
+}
+*/
 export const asignarChofer = async (token) => {
     const endpoint = backendUrl + '/chofer/verChofers';
-    return await executeFetch(endpoint, null, HttpMethods.GET, token, 200);
+    return await executeFetch(endpoint, null, HttpMethods.PATCH, token, 200);
 };
 
+/*
+public class ChoferEditDTO {
+    Integer idChofer;
+}
+
+*/
 export const habilitarChofer = async (token) => {
     const endpoint = backendUrl + '/chofer/verChofers';
-    return await executeFetch(endpoint, null, HttpMethods.GET, token, 200);
+    return await executeFetch(endpoint, null, HttpMethods.PATCH, token, 200);
 };
 
+/*
+public class ChoferEditDTO {
+    Integer idChofer;
+}
+
+*/
 export const inhabilitarChofer = async (token) => {
     const endpoint = backendUrl + '/chofer/verChofers';
-    return await executeFetch(endpoint, null, HttpMethods.GET, token, 200);
+    return await executeFetch(endpoint, null, HttpMethods.PATCH, token, 200);
 };
 
 export const verChoferes = async (token) => {
