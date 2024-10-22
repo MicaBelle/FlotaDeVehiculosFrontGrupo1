aeafed8f8e0e335488b83a241d5bb79baba81430
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-10-22 09:19:30 -0300

    lo deje como antes

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,5 +1,1 @@
-//export const backendUrl = import.meta.env.VITE_API_URL
-export const backendUrl = "http://3.80.97.251:8080"
-export const metaBaseUrl = "http://54.175.63.39:3000"
-//VITE_API_URL=http://3.80.97.251:8080
-//VITE_METABASE_URL=http://54.175.63.39:3000
+export const backendUrl = import.meta.env.VITE_API_URL
